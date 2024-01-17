33c592c8a2b1da0dd87c0c303c4598a43282a7cb
Misi11 <mlc.gluhovic@gmail.com>
2024-01-17 07:36:04 +0100

    Dopuna autentifikacije i funkcionalnosti kroz api i kontroler

--- laravelBackend/routes/api.php
+++ laravelBackend/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\API\AuthController;
 use App\Http\Controllers\DestinacijaController;
 use App\Http\Controllers\PutnikController;
 use App\Http\Controllers\RezervacijaController;
@@ -17,21 +18,31 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('/registracija', [AuthController::class, 'registracija']);
+Route::post('/prijava', [AuthController::class, 'prijava']);
+
+Route::group(['middleware' => ['auth:sanctum']], function () {
+
+    Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+        return $request->user();
+    });
+
+    Route::resource('putniks', PutnikController::class);
+    Route::get('/putniks', [PutnikController::class, 'getAllPutniks']);
+    Route::post('/putniks', [PutnikController::class, 'dodajPutnika']);
+    Route::delete('/putniks/{putnik}', [PutnikController::class, 'obrisiPutnika']);
+
+    Route::resource('rezervacijas', RezervacijaController::class);
+    Route::get('/rezervacijas', [RezervacijaController::class, 'getAllRezervacijas']);
+    Route::post('/rezervacijas', [RezervacijaController::class, 'dodajRezervaciju']);
+    Route::delete('/rezervacijas/{rezervacija}', [RezervacijaController::class, 'obrisiRezervaciju']);
+
+    Route::resource('destinacijas', DestinacijaController::class);
+    Route::post('/destinacijas', [DestinacijaController::class, 'dodajDestinacije']);
+    Route::delete('/destinacijas/{destinacija}', [DestinacijaController::class, 'obrisiDestinacije']);
+
+    Route::post('/odjava', [AuthController::class, 'odjava']);
+
 });
 
-Route::resource('putniks', PutnikController::class);
-Route::get('/putniks', [PutnikController::class, 'getAllPutniks']);
-Route::post('/putniks', [PutnikController::class, 'dodajPutnika']);
-Route::delete('/putniks/{putnik}', [PutnikController::class, 'obrisiPutnika']);
-
-Route::resource('rezervacijas', RezervacijaController::class);
-Route::get('/rezervacijas', [RezervacijaController::class, 'getAllRezervacijas']);
-Route::post('/rezervacijas', [RezervacijaController::class, 'dodajRezervaciju']);
-Route::delete('/rezervacijas/{rezervacija}', [RezervacijaController::class, 'obrisiRezervaciju']);
-
-Route::resource('destinacijas', DestinacijaController::class);
 Route::get('/destinacijas', [DestinacijaController::class, 'getAllDestinacijas']);
-Route::post('/destinacijas', [DestinacijaController::class, 'dodajDestinacije']);
-Route::delete('/destinacijas/{destinacija}', [DestinacijaController::class, 'obrisiDestinacije']);
