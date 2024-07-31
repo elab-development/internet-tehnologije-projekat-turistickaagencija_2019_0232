ddbeaa80eeb67d073e92c81e666c756940835829
anja73369 <anja73369@gmail.com>
2024-07-31 09:35:39 +0100

    Generisanje komponenti Accomodation, Bookings, NavBar

---
+++ angularFront/src/app/components/accommodations/accommodations.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-accommodations',
+  templateUrl: './accommodations.component.html',
+  styleUrls: ['./accommodations.component.scss']
+})
+export class AccommodationsComponent {
+
+}
