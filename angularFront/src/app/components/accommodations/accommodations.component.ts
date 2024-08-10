d4ba9a4a26981b94193b5f93f36818f64c724eda
Misi11 <mlc.gluhovic@gmail.com>
2024-08-10 10:41:20 +0100

    Azuriranje komponenti Accommodation i AccommodationCard

--- angularFront/src/app/components/accommodations/accommodations.component.ts
+++ angularFront/src/app/components/accommodations/accommodations.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Accommodation } from 'src/app/model/accommodation';
+import { Booking } from 'src/app/model/booking';
+import { AccommodationService } from 'src/app/services/accommodation.service';
+import { BookingService } from 'src/app/services/booking.service';
 
 @Component({
   selector: 'app-accommodations',
   templateUrl: './accommodations.component.html',
   styleUrls: ['./accommodations.component.scss']
 })
-export class AccommodationsComponent {
+export class AccommodationsComponent implements OnInit {
+  accommodations: Accommodation[] = [];
 
+  constructor(private accommodationService: AccommodationService,
+              private bookingService: BookingService) {
+  }
+
+  ngOnInit(): void {
+    this.accommodations = this.accommodationService.accommodations;
+  }
+
+  handleAccommodationBooked(accommodation: Accommodation): void {
+    const booking: Booking = {
+      id: this.bookingService.bookings.length + 1,
+      dateFrom: new Date(),
+      dateTo: new Date(),
+      numberOfPersons: 3,
+      price: accommodation.pricePerPerson * 3,
+      accommodation: accommodation
+    };
+    this.bookingService.addBooking(booking);
+  }
 }
