5efe4622a9ca79e3df430d4ceff6c934d3404313
Misi11 <mlc.gluhovic@gmail.com>
2024-08-12 22:43:22 +0100

    Dodatak eksternog API-ja - OpenWeatherAPI

--- angularFront/src/app/app.module.ts
+++ angularFront/src/app/app.module.ts
@@ -3,30 +3,35 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AccommodationsComponent } from './components/accommodations/accommodations.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
-import { TableModule } from 'primeng/table';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AccommodationsComponent } from './components/accommodations/accommodations.component';
+import { BookingsComponent } from './components/bookings/bookings.component';
 import { AccommodationCardComponent } from './components/accommodation-card/accommodation-card.component';
-import { FrameDirective } from './directives/frame.directive'; 
+import { FrameDirective } from './directives/frame.directive';
+import {TableModule} from 'primeng/table';
 import { TaxPipe } from './pipes/tax.pipe';
-
+import { WeatherComponent } from './components/weather/weather.component';
+import { HttpClientModule } from '@angular/common/http';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    AccommodationsComponent,
     NavBarComponent,
+    AccommodationsComponent,
+    BookingsComponent,
     AccommodationCardComponent,
     FrameDirective,
     TaxPipe,
-
-
+    WeatherComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    TableModule    
+    BrowserAnimationsModule,
+    TableModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
