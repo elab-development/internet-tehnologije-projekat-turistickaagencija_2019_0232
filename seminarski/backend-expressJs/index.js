56d88bad4fd3f152d0555ff8dc1faacf85c6b881
Misi11 <mlc.gluhovic@gmail.com>
2024-08-18 00:25:43 +0100

    Obrada funkcionalnosti za upload fotografija preko prosledjenog linka

--- seminarski/backend-expressJs/index.js
+++ seminarski/backend-expressJs/index.js
@@ -5,17 +5,20 @@ const mongoose = require('mongoose');
 
 const bcrypt = require('bcryptjs');
 const bcryptSalt = bcrypt.genSaltSync(10);
-const jwtSecret = 'tajnasifrasdasdanfsdfg';
+const jwtSecret = 'fasefraw4r5r3wq45wdfgw34twdfg';
 
 const User = require('./models/User.js');
 
 const cookieParser = require('cookie-parser');
 
+const imageDownloader = require('image-downloader');
+
 require('dotenv').config();
 const app = express();
 
 app.use(express.json());
-app.use(cookieParser())
+app.use('/uploads', express.static(__dirname+'/uploads'));
+app.use(cookieParser());
 app.use(cors({
     credentials: true,
     origin:'http://localhost:5173',
@@ -85,7 +88,18 @@ app.get('/api/profile', (req,res) => {
 });
 
 app.post('/api/logout', (req,res) => {
-    res.cookie('token', '').json(true);
+  res.cookie('token', '').json(true);
+});
+
+app.post('/api/upload-by-link', async (req,res) => {
+  const {link} = req.body;
+  const newName = 'photo' + Date.now() + '.jpg';
+  await imageDownloader.image({
+    url: link,
+    dest: '/tmp/' +newName,
   });
+  const url = await uploadToS3('/tmp/' +newName, newName, mime.lookup('/tmp/' +newName));
+  res.json(url);
+});
 
 app.listen(4000);
