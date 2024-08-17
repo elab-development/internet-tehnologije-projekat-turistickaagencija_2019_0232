b5ef057cf461aa8c9edb5d572e7c78e7c39e7439
Misi11 <mlc.gluhovic@gmail.com>
2024-08-18 00:13:15 +0100

    Obrada funkcionalnosti autentifikacije

--- seminarski/frontend-react/src/App.jsx
+++ seminarski/frontend-react/src/App.jsx
@@ -6,12 +6,14 @@ import Layout from "./Layout";
 
 function App() {
     return (
-        <Routes>
-            <Route path="/" element={<Layout />}>
-                <Route index element={<IndexPage />} />
-                <Route path="/login" element={<LoginPage />} />
-                <Route path="/register" element={<RegisterPage />} />
-            </Route>
-        </Routes>
+      <UserContextProvider>
+      <Routes>
+          <Route path="/" element={<Layout />}>
+              <Route index element={<IndexPage />} />
+              <Route path="/login" element={<LoginPage />} />
+              <Route path="/register" element={<RegisterPage />} />
+          </Route>
+      </Routes>
+  </UserContextProvider>
             );
 }
