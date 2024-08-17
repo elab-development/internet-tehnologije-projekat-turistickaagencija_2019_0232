20aeeb7260bcf82e9a6db0b1a5688fadc3b2efe0
Misi11 <mlc.gluhovic@gmail.com>
2024-08-18 00:22:04 +0100

    Dodate stranice za prikaz smestaja i dodavanje novog

--- seminarski/frontend-react/src/App.jsx
+++ seminarski/frontend-react/src/App.jsx
@@ -4,6 +4,8 @@ import IndexPage from "./pages/IndexPage";
 import LoginPage from "./pages/LoginPage";
 import Layout from "./Layout";
 import ProfilePage from "./pages/ProfilePage";
+import PlacesFormPage from "./pages/PlacesFormPage";
+import PlacesPage from "./pages/PlacesPage";
 
 function App() {
     return (
@@ -14,6 +16,15 @@ function App() {
               <Route path="/login" element={<LoginPage />} />
               <Route path="/register" element={<RegisterPage />} />
               <Route path="/account" element={<ProfilePage />} />
+              <Route path="/account/places" element={<PlacesPage />} />
+                    <Route
+                        path="/account/places/new"
+                        element={<PlacesFormPage />}
+                    />
+                    <Route
+                        path="/account/places/:id"
+                        element={<PlacesFormPage />}
+                    />
           </Route>
       </Routes>
   </UserContextProvider>
