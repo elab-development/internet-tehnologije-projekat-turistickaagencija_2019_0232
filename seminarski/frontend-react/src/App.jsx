06855b9bf3b6bc6e365c7e60b26b2770f8f8de69
anja73369 <anja73369@gmail.com>
2024-08-18 06:06:15 +0100

    Obradjivanje detaljnog prikaza smestaja

--- seminarski/frontend-react/src/App.jsx
+++ seminarski/frontend-react/src/App.jsx
@@ -2,21 +2,28 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import IndexPage from "./pages/IndexPage";
 import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
 import Layout from "./Layout";
+import axios from "axios";
+import { UserContextProvider } from "./UserContext";
 import ProfilePage from "./pages/ProfilePage";
 import PlacesFormPage from "./pages/PlacesFormPage";
 import PlacesPage from "./pages/PlacesPage";
+import PlacePage from "./pages/PlacePage";
+
+axios.defaults.baseURL = "http://localhost:4000";
+axios.defaults.withCredentials = true;
 
 function App() {
     return (
-      <UserContextProvider>
-      <Routes>
-          <Route path="/" element={<Layout />}>
-              <Route index element={<IndexPage />} />
-              <Route path="/login" element={<LoginPage />} />
-              <Route path="/register" element={<RegisterPage />} />
-              <Route path="/account" element={<ProfilePage />} />
-              <Route path="/account/places" element={<PlacesPage />} />
+        <UserContextProvider>
+            <Routes>
+                <Route path="/" element={<Layout />}>
+                    <Route index element={<IndexPage />} />
+                    <Route path="/login" element={<LoginPage />} />
+                    <Route path="/register" element={<RegisterPage />} />
+                    <Route path="/account" element={<ProfilePage />} />
+                    <Route path="/account/places" element={<PlacesPage />} />
                     <Route
                         path="/account/places/new"
                         element={<PlacesFormPage />}
@@ -25,8 +32,11 @@ function App() {
                         path="/account/places/:id"
                         element={<PlacesFormPage />}
                     />
-          </Route>
-      </Routes>
-  </UserContextProvider>
-            );
+                    <Route path="/place/:id" element={<PlacePage />} />
+                </Route>
+            </Routes>
+        </UserContextProvider>
+    );
 }
+
+export default App;
